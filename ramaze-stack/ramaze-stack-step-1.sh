5a8d88f0e1c9b1a1c8d511055133e1f2660ed16e
Pistos <gitsomegrace.5.pistos@geoshell.com>
2008-12-11 23:10:34 -0500

    Filling out stack script.

--- ramaze-stack/ramaze-stack-step-1.sh
+++ ramaze-stack/ramaze-stack-step-1.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+#EMERGE_OPTIONS="-tv"
+EMERGE_OPTIONS="-atv"
+
 cat sources/etc/make.conf.appendum >> /etc/make.conf
 cp sources/etc/portage/* /etc/portage
+
+emerge ${EMERGE_OPTIONS} "=app-shells/bash-3.2_p17-r1" portage
+
+echo 'rm_opts=""' >> /etc/etc-update.conf
+echo 'mv_opts=""' >> /etc/etc-update.conf
+echo 'cp_opts=""' >> /etc/etc-update.conf
+
+emerge ${EMERGE_OPTIONS} syslog-ng logrotate
+emerge ${EMERGE_OPTIONS} ruby postgresql nginx
+
