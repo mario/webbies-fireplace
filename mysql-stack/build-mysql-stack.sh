24fb5f9a826650fb37c05afc1b9aa26d34903282
unknown <mario.danic@gmail.com>
2008-12-15 07:34:17 +0100

    Initial mysql stack import

---
+++ mysql-stack/build-mysql-stack.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Update list of available packages
+
+echo 'Fetching updated list of available packages'
+apt-get update
+echo 'Done'
+
+# Somebody missed essential packages
+
+echo 'Fetching and installing essential log packages'
+apt-get install syslog-ng logrotate -y
+echo 'Done'
+
+# Making sure mysql wont ask us questions
+echo 'Be silent mysql'
+export DEBIAN_FRONTEND=noninteractive
+echo 'Done'
+
+# Lets install mysql-server
+echo 'Mysql server installation'
+sudo apt-get install mysql-server
+echo 'Done'
+
+# Generate mysql password
+echo 'Generating mysql root pass'
+PASS= < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c8
+echo 'Done'
+
+# Setting up root pass for mysql
+echo 'Setting up root mysql pass'
+mysqladmin -u root -p password $PASS
+echo 'Done'
+
+echo '*****************************************************'
+echo
+echo 'ALL DONE!'
+echo 'Your mysql root password is:'"$PASS"
+echo
+echo '*****************************************************'
