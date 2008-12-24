4b28d56cb1190fc030fdae65117af5ec927f573f
Travis Graham <tgraham@smithandhammaker.com>
2008-12-24 09:37:59 -0500

    Removed accidental duplication, moved php into utils-php.sh

--- utils/utils-apache.sh
+++ utils/utils-apache.sh
@@ -43,21 +43,18 @@ echo 'Install/Remove apache modules'
 
 case "${1}" in
   1)
-    modname=php5-mysql
-    ;;
-  2)
     modname=libapache2-mod-auth-mysql
     ;;
-  3)
+  2)
     modname=libapache2-mod-php5
     ;;
-  4)
+  3)
     modname=libapache2-mod-jk
     ;;
-  5)
+  4)
     modname=libapache2-mod-python
     ;;
-  6)
+  5)
     modname=libapache2-svn
     ;;
   *)
@@ -75,210 +72,6 @@ else
   # installing module
   
   apt-get -y install $modname 
-  
-fi
-
-echo 'Done'
-
-}
-
-apache_module_enable () {
-# enable/disable module
-# Argument $1 is module, Argument $2 is what to do
-
-echo 'Enable/Disable apache modules'
-
-case "${1}" in
-  1)
-    modname=rewrite
-    ;;
-  2)
-    modname=ssl
-    ;;
-  *)
-    echo 'Not an option'
-    exit
-    ;;
-esac
-
-if [ $2 = 1 ]
-  # disable module
-
-  a2dismod $modname
-
-else
-  # enable module
-  
-  a2enmod $modname
-
-fi
-
-echo 'Done'
-
-}
-
-apache_site_enable () {
-# enable/disable site
-# Argument $1 is site, Argument $2 is what to do
-
-echo 'Enable/Disable site'
-
-if [ $2 = 1 ]
-  # disable site
-
-  a2dissite $1
-
-else
-  # enable site
-  
-  a2ensite $1
-
-fi
-
-echo 'Done'
-
-}apache_module_install () {
-# install/remove module
-# Argument $1 is module, Argument $2 is what to do
-
-echo 'Install/Remove apache modules'
-
-case "${1}" in
-  1)
-    modname=php5-mysql
-    ;;
-  2)
-    modname=libapache2-mod-auth-mysql
-    ;;
-  3)
-    modname=libapache2-mod-php5
-    ;;
-  4)
-    modname=libapache2-mod-jk
-    ;;
-  5)
-    modname=libapache2-mod-python
-    ;;
-  6)
-    modname=libapache2-svn
-    ;;
-  *)
-    echo 'Not an option'
-    exit
-    ;;
-esac
-
-if [ $2 = 1 ]
-  # removing module
-  
-  apt-get -y remove $modname
-  
-else
-  # installing module
-  
-  apt-get -y install $modname 
-  
-fi
-
-echo 'Done'
-
-}
-
-apache_module_enable () {
-# enable/disable module
-# Argument $1 is module, Argument $2 is what to do
-
-echo 'Enable/Disable apache modules'
-
-case "${1}" in
-  1)
-    modname=rewrite
-    ;;
-  2)
-    modname=ssl
-    ;;
-  *)
-    echo 'Not an option'
-    exit
-    ;;
-esac
-
-if [ $2 = 1 ]
-  # disable module
-
-  a2dismod $modname
-
-else
-  # enable module
-  
-  a2enmod $modname
-
-fi
-
-echo 'Done'
-
-}
-
-apache_site_enable () {
-# enable/disable site
-# Argument $1 is site, Argument $2 is what to do
-
-echo 'Enable/Disable site'
-
-if [ $2 = 1 ]
-  # disable site
-
-  a2dissite $1
-
-else
-  # enable site
-  
-  a2ensite $1
-
-fi
-
-echo 'Done'
-
-}apache_module_install () {
-# install/remove module
-# Argument $1 is module, Argument $2 is what to do
-
-echo 'Install/Remove apache modules'
-
-case "${1}" in
-  1)
-    modname=php5-mysql
-    ;;
-  2)
-    modname=libapache2-mod-auth-mysql
-    ;;
-  3)
-    modname=libapache2-mod-php5
-    ;;
-  4)
-    modname=libapache2-mod-jk
-    ;;
-  5)
-    modname=libapache2-mod-python
-    ;;
-  6)
-    modname=libapache2-svn
-    ;;
-  *)
-    echo 'Not an option'
-    exit
-    ;;
-esac
-
-if [ $2 = 1 ]
-  # removing module
-  
-  apt-get remove $modname -y
-  
-else
-  # installing module
-  
-  apt-get install $modname -y
   
 fi
 
