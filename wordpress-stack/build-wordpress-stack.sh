51fb5c43c067e1fdc8bd25d9de0a267d91f38f74
Travis Graham <tgraham@smithandhammaker.com>
2008-12-24 15:05:16 -0500

    Using the framework setup to install LAMP stack needed and download WP files.

--- wordpress-stack/build-wordpress-stack.sh
+++ wordpress-stack/build-wordpress-stack.sh
@@ -6,7 +6,9 @@
 #               Because this script creates a db and user MySQL cannot be preinstalled.
 #               If MySQL is installed this script will exit.
 
-if [ which mysqld ]
+which mysql
+
+if [ $? -eq 0 ]; then
   # exit if MySQL is found
   echo 'MySQL is installed, Exiting'
   exit
@@ -17,5 +19,30 @@ source utils-misc.sh
 source utils-apache.sh
 source utils-db.sh
 source utils-php.sh
+
+# once the LAMP Stack is finished this will be replaced
+misc_apt_update
+misc_install_syslog
+ufw_install_configure
+ufw_openssh
+ufw_apache_full
+apache_install_configure
+apache_module_install 1
+php_install 2
+php_module_install 2 1
+db_mysql_install_configure
+apache_module_enable 1
+misc_phpmyadmin_configure
+apache_manage 1
+
+# Download Wordpress Latest
+echo 'Downloading and uncompressing Wordpress'
+mkdir ~/WPinstall
+cd ~/WPinstall
+wget http://wordpress.org/latest.tar.gz
+tar -xzvf latest.tar.gz
+cd wordpress
+echo 'WP downloaded and uncompressed'
+
 
 echo 'Done'
