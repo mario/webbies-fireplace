c28989bd8e035b654569295c72911905189fcca2
Travis Graham <tgraham@smithandhammaker.com>
2008-12-25 17:00:28 -0500

    Trying to fix genpass function

--- utils/utils-misc.sh
+++ utils/utils-misc.sh
@@ -4,12 +4,14 @@
 misc_genpass () {
 # only argument is number of chars password should have
 # please use 16 for DB stuff and 5 for more regular things
-if [ "$1" = ""]; then 
-  $1 = 16
+if [ -n "$1" ]; then 
+  CHARS=$1
+else
+  CHARS=16
 fi
     
 echo 'Generating a password'
-PASS=$(head -c 500 /dev/urandom | tr -dc a-z0-9A-Z | head -c 16)
+PASS=$(head -c 500 /dev/urandom | tr -dc a-z0-9A-Z | head -c $CHARS)
 echo 'Done'
 
 }
