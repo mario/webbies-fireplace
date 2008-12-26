473d9a779fa0a00a989af147a7db74976a837d27
Travis Graham <tgraham@smithandhammaker.com>
2008-12-26 15:28:15 -0500

    First pass at LAMP rebuild

---
+++ lamp-stack/build-lamp-stack2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# build-lamp-stack2.sh: LAMP installer v2.
+
+# Import utils { misc, apache, db, php, ufw } 
+source ../utils/utils-misc.sh
+source ../utils/utils-apache.sh
+source ../utils/utils-db.sh
+source ../utils/utils-php.sh
+source ../utils/utils-ufw.sh
+
+# LAMP stack
+misc_apt_update
+misc_install_syslog
+ufw_install_configure
+ufw_openssh
+ufw_enable
+apache_install_configure
+ufw_apache_full
+apache_module_install 1
+php_install 2
+php_module_install 2 1
+db_mysql_install_configure
+apache_module_enable 1
+misc_phpmyadmin_configure
+apache_manage 1
+
+echo 'Your MySQL root password is '"$PASS"
