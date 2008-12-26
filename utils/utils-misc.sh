b3cff108cb1ceefb8dd2419204d53df60dd394a9
Travis Graham <tgraham@smithandhammaker.com>
2008-12-26 09:36:18 -0500

    removing duplication

--- utils/utils-misc.sh
+++ utils/utils-misc.sh
@@ -35,60 +35,6 @@ apt-get install syslog-ng logrotate -y
 echo 'Done'
 
 }
-misc_apt_update () {
-
-# Update list of available packages
-
-echo 'Fetching updated list of available packages'
-apt-get update
-echo 'Done'
-
-}
-
-misc_install_syslog () {
-
-# Essential packages
-
-echo 'Fetching and installing essential log packages'
-apt-get install syslog-ng logrotate -y
-echo 'Done'
-
-}
-misc_phpmyadmin_configure () {
-
-# Installing phpmysqladmin
-
-echo 'Getting up and ready with phpmysqladmin'
-DEBIAN_FRONTEND=noninteractive apt-get install phpmyadmin -y
-echo 'Done'
-
-# Setting up phpmysqladmin
-
-echo 'Setting up phpmysqladmin'
-echo "Include /etc/phpmyadmin/apache.conf" >> /etc/apache2/apache2.conf
-echo 'Done'
-
-}
-
-misc_apt_update () {
-
-# Update list of available packages
-
-echo 'Fetching updated list of available packages'
-apt-get update
-echo 'Done'
-
-}
-
-misc_install_syslog () {
-
-# Essential packages
-
-echo 'Fetching and installing essential log packages'
-apt-get install syslog-ng logrotate -y
-echo 'Done'
-
-}
 
 misc_phpmyadmin_configure () {
 
