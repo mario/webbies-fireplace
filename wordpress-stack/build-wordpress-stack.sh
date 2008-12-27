2e00bdd23a6f1905aab7dd8a3672c01987489117
Travis Graham <tgraham@smithandhammaker.com>
2008-12-27 14:51:40 -0500

    Working on regex

--- wordpress-stack/build-wordpress-stack.sh
+++ wordpress-stack/build-wordpress-stack.sh
@@ -26,6 +26,7 @@ fi
  USEREMAIL=$1
  REGEX="\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b"
  if [[ "$USEREMAIL" =~ "$REGEX" ]]; then
+   echo 'Email is valid... moving on.'
  else
    echo 'Not a valid email, pleace check it.'
    exit
