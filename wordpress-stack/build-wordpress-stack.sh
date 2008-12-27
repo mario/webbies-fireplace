ade5ce5d46a3d648f6348ffb42609144c2d19852
Travis Graham <tgraham@smithandhammaker.com>
2008-12-26 22:02:00 -0500

    uncomment out email

--- wordpress-stack/build-wordpress-stack.sh
+++ wordpress-stack/build-wordpress-stack.sh
@@ -23,7 +23,7 @@ if [ $# -eq 0 ]; then
 fi
 
 # regex validation for email (inprogress)
-# USEREMAIL=$1
+ USEREMAIL=$1
 # REGEX="\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b"
 # if [ $USEREMAIL =~ $REGEX ]; then
   # stuff
